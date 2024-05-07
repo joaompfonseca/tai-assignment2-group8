ab43fab4b68b1dba615fbe1fcd9fc1d32662a7b8
Rafael Fernandes Gonçalves <rfg@ua.pt>
2024-05-07 23:30:58 +0100

    Update bench

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,6 @@
 FOLDS="0" #1 2 3 4 5 6 7 8 9"
+# K_VALUES="6"
+# SMOOTHING_VALUES="0.1"
 DATASET="datasets/out/wiki"
 
 cd bin
@@ -7,8 +9,11 @@ rm -rf ../$DATASET/run
 mkdir ../$DATASET/run
 
 for fold in $FOLDS; do
+    rm ../$DATASET/train/0/train$fold.txt.order6.cache
+    rm ../$DATASET/train/1/train$fold.txt.order6.cache
+
     touch ../$DATASET/run/run$fold.txt
     cat ../$DATASET/val/0/val$fold.txt ../$DATASET/val/1/val$fold.txt > ../$DATASET/run/run$fold.txt
 
-    ./was_chatted -n ../$DATASET/train/0/train$fold.txt -r ../$DATASET/train/1/train$fold.txt -t ../$DATASET/run/run$fold.txt -l ../$DATASET/run/out$fold.csv -a ../example/alphabet.txt -k 6 -s 0.1
+    ./was_chatted -n ../$DATASET/train/0/train$fold.txt -r ../$DATASET/train/1/train$fold.txt -t ../$DATASET/run/run$fold.txt -l ../$DATASET/run/out$fold.csv -a ../example/alphabet.txt -k 6 -s 1.0 -d 1.0
 done
